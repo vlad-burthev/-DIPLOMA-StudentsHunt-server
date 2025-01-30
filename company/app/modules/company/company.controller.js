4b5a5d3ebd9adc5235553969426a5646c36b345e
vladburthev <burthev04@gmail.com>
2025-01-30 14:28:39 +0200

    create company

--- company/app/modules/company/company.controller.js
+++ company/app/modules/company/company.controller.js
@@ -6,24 +6,37 @@ import client from "../../../db.config.js";
 import * as uuid from "uuid";
 import * as bcrypt from "bcrypt";
 import CloudinaryService from "../../../../services/cloudnary.service.js";
+import { checkEGRPOUCode } from "../../../../services/egrpou.service.js";
 
 export class CompanyController {
   static async createCompany(req, res, next) {
     try {
       const queryGetCompanyByParams = `
         SELECT * FROM companies 
-        WHERE email = $1 OR title = $2
+        WHERE email = $1 OR title = $2 OR egrpou = $3
       `;
 
       const { rowCount } = await client.query(queryGetCompanyByParams, [
         req.body.email,
         req.body.title,
+        req.body.egrpou,
       ]);
 
       if (rowCount > 0) {
         return next(
           ApiError.UNAUTHORIZED("Компанія з таким email або назвою вже існує ")
         );
+      }
+
+      const egrpouInfo = await checkEGRPOUCode(req.body.egrpou);
+
+      if (egrpouInfo instanceof Error) {
+        return next(ApiError.BAD_REQUEST(egrpouInfo.message));
+      }
+
+      if (!req.file) {
+        console.log(req.file);
+        return next(ApiError.BAD_REQUEST("File path is missing or invalid"));
       }
 
       let photoUrl = null;
@@ -34,7 +47,6 @@ export class CompanyController {
             "company",
             "image"
           );
-
           photoUrl = result.secure_url;
         } catch (uploadError) {
           return next(ApiError.INTERNAL_SERVER_ERROR("Ошибка загрузки фото"));
@@ -62,7 +74,6 @@ export class CompanyController {
 
       const { rows } = await client.query(queryInsertCompany, values);
 
-      // Успешный ответ
       return ApiResponse.OK(res, rows[0]);
     } catch (error) {
       console.log(error.message);
